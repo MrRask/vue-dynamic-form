1626cf091b738696f0e66f326e08dbbc7f21b4fb
Parnupat Jansiri <parnupat.j@gmail.com>
2019-11-15 23:09:23 +0700

    set local

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import locale from 'element-ui/lib/locale/lang/th'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'flexboxgrid'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 new Vue({
   router,
   store,

4dab7e9d19aca55e9d0e7623fe78bc2ca8636712
Parnupat Jansiri <parnupat.j@gmail.com>
2019-11-15 23:09:49 +0700

    reuse component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="home"></div>
+  <div class="container">
+    <h1>This page for form generate</h1>
+    <div class="row">
+      <div class="col-xs-6">
+        <form-schema @create="addSchema"></form-schema>
+      </div>
+      <div class="col-xs-6">
+        <form-generator :schema="schema"></form-generator>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import FormSchema from '@/components/form-schema'
+import FormGenerator from '@/components/form-generator'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    FormGenerator,
+    FormSchema
+  },
+  data () {
+    return {
+      form: {},
+      schema: []
+    }
+  },
+  created () {
+    // set structor
+    var obj = {}
+    this.schema.map((item) => {
+      if (item.type === 'el-checkbox-group') { obj[item.model] = [] } else obj[item.model] = null
+    })
+    this.form = obj
+  },
+  beforeDestroy () {
+    console.log('DESTROYYYY!!!')
+  },
+  methods: {
+    addSchema (val) {
+      this.schema.push(val)
+      console.log(this.schema)
+    }
+  }
 }
 </script>
